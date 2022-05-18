35732b4be8fabfbb77a19cc8e8d48b14ab216cf8
mehdi besliu <mehdi.besliu@yahoo.com>
2022-05-18 12:55:27 +0200

    Yealp easy

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,31 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new # needed to instantiate the form_for
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    # no need for app/views/restaurants/create.html.erb
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+end
